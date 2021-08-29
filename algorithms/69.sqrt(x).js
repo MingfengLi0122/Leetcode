9ac589e06df6b04bdd6712f22d17250488fbe705
MingfengLi0122 <lmf950122@gmail.com>
2021-08-28 23:28:34 -0700

    Practice sqrt(x) problem from leetcode

---
+++ algorithms/69.sqrt(x).js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var mySqrt = function(x) {
+  let right = x / 2 + 1;
+  let left = 1;
+  let mid;
+  while(left <= right) {
+      mid = Math.floor((left + right) / 2);
+      if(mid*mid > x) {
+          right = mid - 1;
+      } else if (mid*mid < x) {
+          left = mid + 1;
+      } else {
+          return mid;
+      }
+  }
+  return right;
+};
