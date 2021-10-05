5330af329367939fbc3924628fb8edd0e7afd999
MingfengLi0122 <lmf950122@gmail.com>
2021-10-04 23:05:02 -0700

    Practice pratition equal subset sum question from leetcode

---
+++ algorithms/416.partition-equal-subset-sum.js
@@ -0,0 +1,12 @@
+var canPartition = function(nums) {
+  var sum = nums.reduce((a, b) => a + b);
+  if(sum % 2 === 1) return false;
+  var target = sum / 2;
+  var dp = new Array(target + 1).fill(0);
+  for(var i = 0; i < nums.length; i++) {
+      for(var j = target; j >= nums[i]; j--) {
+          dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+      }
+  }
+  return dp[target] === target;
+};
