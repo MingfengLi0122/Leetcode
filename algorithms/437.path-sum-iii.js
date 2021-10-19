1c8642f85b9afb1fbea5a2a554cbfc6d65ecf24f
MingfengLi0122 <lmf950122@gmail.com>
2021-10-18 23:38:41 -0700

    Practice path sum iii question from leetcode

---
+++ algorithms/437.path-sum-iii.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+var pathSum = function (root, targetSum) {
+  if (root === null) return 0;
+
+  return pathSum(root.left, targetSum) + pathSum(root.right, targetSum) + pathSum2(root, targetSum);
+};
+
+const pathSum2 = function (node, targetSum) {
+  if (node === null) return 0;
+
+  let count = 0;
+
+  if (node.val === targetSum) count = 1;
+
+  let newSum = targetSum - node.val;
+  count += pathSum2(node.left, newSum);
+  count += pathSum2(node.right, newSum);
+  return count;
+}
