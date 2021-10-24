bf0e9ab53b93a32e359c24156e86348e3076809d
MingfengLi0122 <lmf950122@gmail.com>
2021-10-23 23:52:45 -0700

    Update helper function structure

--- algorithms/79.word-search.js
+++ algorithms/79.word-search.js
@@ -4,32 +4,32 @@
  * @return {boolean}
  */
 
-const isOutOfBound = (board, row, col) => row < 0 || row >= board.length || col < 0 || col >= board[0].length;
-
-function existHelper(board, word, row, col) {
-    if (!word.length) return true;
-    if (isOutOfBound(board, row, col) || board[row][col] !== word[0]) return false;
-
-    const curChar = board[row][col];
-    const newWord = word.substr(1);
-
-    board[row][col] = 0;
-
-    const results = existHelper(board, newWord, row + 1, col) ||
-        existHelper(board, newWord, row - 1, col) ||
-        existHelper(board, newWord, row, col + 1) ||
-        existHelper(board, newWord, row, col - 1)
-
-    board[row][col] = curChar;
-
-    return results;
-}
-
-var exist = function (board, word) {
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[0].length; col++) {
-            if (existHelper(board, word, row, col)) return true;
-        }
-    }
-    return false;
-};
+ function isOutOfBound(board, row, col) {
+    return row < 0 || row >= board.length || col < 0 || col >= board[0].length;
+ } 
+ 
+ function existHelper(row, col, word, board) {
+     if (!word.length) return true;
+     if (isOutOfBound(board, row, col) || board[row][col] !== word[0]) return false; 
+     
+     const curChar = board[row][col];
+     const newWord = word.substr(1);
+     board[row][col] = 0;
+     
+     const results = existHelper(row + 1, col, newWord, board) ||
+           existHelper(row - 1, col, newWord, board) ||
+           existHelper(row, col + 1, newWord, board) ||
+           existHelper(row, col - 1, newWord, board)
+     board[row][col] = curChar;
+     
+     return results;
+ }
+ 
+ var exist = function(board, word) {
+     for (let row = 0; row < board.length; row++) {
+         for (let col = 0; col < board[0].length; col++) {
+             if (existHelper(row, col, word, board)) return true;
+         }
+     }
+     return false;
+ };
