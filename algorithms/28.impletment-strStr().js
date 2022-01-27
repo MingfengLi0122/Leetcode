88ad738404d2101ef5eb7c36e58d58a8b9154810
Mingfeng Li <lmf950122@gmail.com>
2022-01-26 22:15:37 -0800

    Apply second method for question 28.implement strStr() from leetcode

--- algorithms/28.impletment-strStr().js
+++ algorithms/28.impletment-strStr().js
@@ -22,3 +22,27 @@
   
   return -1;
 };
+
+
+// method 2
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+  if (needle === '' || needle === haystack) return 0;
+  
+  if (needle.length > haystack.length) return -1;
+  
+  for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+      if (haystack[i] === needle[0]) {
+          for (let j = 0; j < needle.length; j++) {
+              if (haystack[i + j] !== needle[j]) break;
+              else if (j === needle.length - 1) return i;
+          }
+      }
+  }
+  
+  return -1;
+};
