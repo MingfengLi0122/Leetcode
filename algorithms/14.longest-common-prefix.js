0ad576718bda288b4e29d29d8d6ad88ea5264fd4
Mingfeng Li <lmf950122@gmail.com>
2022-01-28 01:15:55 -0800

    Repractice question 14.longest common prefix question from leetcode

--- algorithms/14.longest-common-prefix.js
+++ algorithms/14.longest-common-prefix.js
@@ -3,17 +3,19 @@
  * @return {string}
  */
  var longestCommonPrefix = function(strs) {
-  if (!strs.length) return '';
-  let prefix = '';
-  let maxPrefixLength = Math.min(...strs.map(str => str.length));
-  
-  for (let i = 0; i < maxPrefixLength; i++) {
-      let char = strs[0][i];
-      if (strs.every(str => str[i] === char)){
-          prefix += char;
-      } else {
-          break;
-      }
-  }
-  return prefix;
+    if (!strs.length) return '';
+    
+    let prefix = '';
+    let maxPrefixLength = Math.min(...strs.map(str => str.length));
+    
+    for (let i = 0; i < maxPrefixLength; i++) {
+        let char = strs[0][i];
+        
+        if (strs.every(str => str[i] === char)) {
+            prefix += char;
+        } else {
+            break;
+        }
+    }
+    return prefix;
 };
