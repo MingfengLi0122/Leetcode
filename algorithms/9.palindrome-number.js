9c5c8192c0f8b8cf57c15c08a414a5532d00c649
Mingfeng Li <lmf950122@gmail.com>
2022-01-29 21:06:39 -0800

    Repractice question 0. palindorm number from leetocode

--- algorithms/9.palindrome-number.js
+++ algorithms/9.palindrome-number.js
@@ -2,13 +2,13 @@
  * @param {number} x
  * @return {boolean}
  */
-/*Given an integer x, return true if x is palindrome integer.
-
-An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not. */
-
-var isPalindrome = function (x) {
+ var isPalindrome = function(x) {
   if (x < 0) return false;
+  
   let rev = 0;
-  for (let i = x; i >= 1; i = Math.floor(i / 10)) rev = rev * 10 + i % 10;
-  return rev === x;
+  
+  for (let i = x; i >= 1; i = Math.floor(i / 10)) {
+      rev = rev * 10 + i % 10;
+  }
+  return rev;
 };
