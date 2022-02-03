a51c644badd50ec3595d483dd30a827c6908ac76
Mingfeng Li <lmf950122@gmail.com>
2022-02-02 22:31:27 -0800

    Repractice question 35. search insert postion from leetcode

--- algorithms/35.search-insert-position.js
+++ algorithms/35.search-insert-position.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
  var searchInsert = function(nums, target) {
-  let start = 0;
-  let end = nums.length - 1;
-  
-  while(start <= end) {
-      let mid = Math.floor(start + end);
-      if (nums[mid] === target) return mid;
-      if(nums[mid] > target) {
-          end = mid - 1;
-      }
-      if(nums[mid] < target)  {
-          start = mid + 1
-      }
-  }
-  return start;
+    let start = 0;
+    let end = nums.length - 1;
+    
+   while(start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        
+        if (nums[mid] === target) return mid;
+        
+        if (nums[mid] < target) start = mid + 1;
+        
+        if (nums[mid] > target) end = mid - 1;
+    }
+    
+    return start;
 };
