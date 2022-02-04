7066fecbb45be4b35adf043c4ef646c0e89a09c7
Mingfeng Li <lmf950122@gmail.com>
2022-02-03 20:55:48 -0800

    Repractice question 121.best time to buy and sell stock from leetcode

--- algorithms/121.best-time-buy-and-sell-stock.js
+++ algorithms/121.best-time-buy-and-sell-stock.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
  var maxProfit = function(prices) {
-  let min = prices[0];
-  let max = 0;
-  
-  for (let i = 0; i < prices.length; i++) {
-      if (prices[i] - min > max) {
-          max = prices[i]  - min;
-      } 
-      
-      if (prices[i] < min) min = prices[i];
-  }
-  return max;
+    let min = prices[0];
+    let max = 0;
+    
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] - min > max) max = prices[i] - min;
+        
+        if (prices[i] < min) min = prices[i];
+    }
+    
+    return max;
 };
