2de32ad081018ade46795c5e566fcc131cfa0241
Mingfeng Li <lmf950122@gmail.com>
2022-02-08 22:51:53 -0800

    Repractice question 283. move zeros from leetcode

--- algorithms/283.move-zeros.js
+++ algorithms/283.move-zeros.js
@@ -2,12 +2,14 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- var moveZeroes = function(nums) {
-  for (let i = 0, j = 0; i < nums.length, j < nums.length; j++) {
-      if (nums[j] !== 0) {
-          let temp = nums[i];
-          nums[i++] = nums[j];
-          nums[j] = temp;
-      }
-  }
-};  
+var moveZeroes = function (nums) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            nums[i] = index === i ? nums[i] : 0;
+            index++;
+        }
+    }
+};
