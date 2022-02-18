70bce038adb6846c4d39d6232ffea7b004cbe891
Mingfeng Li <lmf950122@gmail.com>
2022-02-17 22:27:18 -0800

    Repractice question 3.longest substring without repeating charcters from leetcode

--- algorithms/3.longest-substring-without-repeating.js
+++ algorithms/3.longest-substring-without-repeating.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-  let left_pointer = 0;
-  let right_pointer = 0;
-  let max = 0;
-  let set = new Set();
-  
-  while (right_pointer < s.length) {
-      if (!set.has(s.charAt(right_pointer))) {
-          set.add(s.charAt(right_pointer));
-          right_pointer++;
-          max = Math.max(max, set.size);
-      } else {
-          set.delete(s.charAt(left_pointer));
-          left_pointer++;
-      }
-  }
-  return max;
+    let max = 0;
+    let begin = 0;
+    let map = {};
+    
+    for (let end = 0; end < s.length; end++) {
+        if (map[s[end]] !== undefined && map[s[end]] >= begin) {
+            begin = map[s[end]] + 1;
+        }
+        map[s[end]] = end;
+        max = Math.max(max, end - begin + 1);
+    }
+    
+    return max;
 };
